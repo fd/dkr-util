c5e618cf81553918e9273455e89787e88c61285a
Simon Menke <simon.menke@gmail.com>
2016-05-03 13:33:56 +0200

    Fixed head requests

--- vendor/github.com/heroku/docker-registry-client/registry/errortransport.go
+++ vendor/github.com/heroku/docker-registry-client/registry/errortransport.go
@@ -1,6 +1,6 @@
 package registry
 
-import(
+import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,7 +8,7 @@ import(
 
 type HttpStatusError struct {
 	Response *http.Response
-	Body     []byte           // Copied from `Response.Body` to avoid problems with unclosed bodies later. Nobody calls `err.Response.Body.Close()`, ever.
+	Body     []byte // Copied from `Response.Body` to avoid problems with unclosed bodies later. Nobody calls `err.Response.Body.Close()`, ever.
 }
 
 func (err *HttpStatusError) Error() string {
@@ -29,14 +29,18 @@ func (t *ErrorTransport) RoundTrip(request *http.Request) (*http.Response, error
 
 	if resp.StatusCode >= 400 {
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("http: failed to read response body (status=%v, err=%q)", resp.StatusCode, err)
+
+		var body []byte
+		if request.Method != "HEAD" {
+			body, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return nil, fmt.Errorf("http: failed to read response body (status=%v, err=%q)", resp.StatusCode, err)
+			}
 		}
 
-		return nil, &HttpStatusError {
+		return nil, &HttpStatusError{
 			Response: resp,
-			Body: body,
+			Body:     body,
 		}
 	}
 
